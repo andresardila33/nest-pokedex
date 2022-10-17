2f83df1b6f1f817f7f55714ef42aad05e5bfca2a
Andres Ardila <andresardila33@gmail.com>
2022-10-16 20:09:42 -0500

    Insertar multiples registros simultáneamente

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
+    const pokemonToInsert: { name: string; no: number }[] = [];
     data.results.forEach(({ url, name }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({ name, no });
+      //const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no });
     });
-    return data.results;
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    return { message: `Seed executed successfully` };
   }
 }
